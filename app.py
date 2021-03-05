983825a0efc9e51769649c9bd47f888f2eedfe46
Andres <andres-gaudioso@outlook.es>
2021-03-05 21:28:58 +0100

    Haciendo peticiones REST a swapi

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print ('Hola caracola')
+print ('Hola cazando apis')
+
+import requests
+import json
+
+response = requests.get("https://swapi.dev/api/people/2")
+
+print (response.text)
+
+json_data = json.loads(response.text)
+
+print (json_data['name'])
