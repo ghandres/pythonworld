f1843131b70f0d0417281dc6841c527577330be1
Andres <andres-gaudioso@outlook.es>
2021-03-05 23:19:59 +0100

    primeros paso con peticiones post

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return '<h1>Hello World</h1>'
+def welcome():
+    print(url_for('welcome'))
+    return '<h1>Welcome</h1>'
+
+
+@app.route('/hello/')
+@app.route('/hello/<string:name>')
+def hello(name=None):
+    if name:
+        return f'<h1>Hello World {name} </h1>'
+    else:
+        return '<h1>Hello Mundo Nada </h1>'
+
+
+@app.route('/posteando/', methods=['POST', 'GET'])
+def post():
+    if request.method == 'GET':
+        return "A la pantalla de inicio"
+    else:
+        return "Llega el formulario"
 
 
 if __name__ == '__main__':
